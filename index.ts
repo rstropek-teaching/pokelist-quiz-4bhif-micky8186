c275a7661e852aa5ff9bb4f1a538e3a9b9a36df8
micky8186 <micky8186@gmx.at>
2017-11-26 10:20:34 +0100

    a little bit of cleanup

--- index.ts
+++ index.ts
@@ -1,21 +1,40 @@
 let page:number = 0;
-let limit:number = 20;
-let pokemonlist:HTMLElement = $("#pokemonlist")[0];
-let prevpagebutton:HTMLButtonElement = <HTMLButtonElement>$("#prevpage")[0];
-let nextpagebutton:HTMLButtonElement = <HTMLButtonElement>$("#nextpage")[0];
-let returnToOverviewButton:HTMLButtonElement = <HTMLButtonElement>$("#rto")[0];
-let namerow:HTMLTableRowElement = <HTMLTableRowElement>$("#namerow")[0];
-let weightrow:HTMLTableRowElement = <HTMLTableRowElement>$("#weightrow")[0];
-let imagerow:HTMLTableRowElement = <HTMLTableRowElement>$("#imagerow")[0];
-let abilityrow:HTMLTableRowElement = <HTMLTableRowElement>$("#abilityrow")[0];
+const limit:number = 20;
+let pokemonlist:HTMLElement;
+let prevpagebutton:HTMLButtonElement;
+let nextpagebutton:HTMLButtonElement;
+let returnToOverviewButton:HTMLButtonElement;
+let namerow:HTMLTableRowElement;
+let weightrow:HTMLTableRowElement;
+let imagerow:HTMLTableRowElement;
+let abilityrow:HTMLTableRowElement;
 
-async function getdetails(url:string){
-    const details = await $.get(url);
+function hidePokemonlist():void{
     pokemonlist.hidden = true;
     prevpagebutton.hidden = true;
     nextpagebutton.hidden = true;
+}
+function showPokemonlist():void{
+    pokemonlist.hidden = false;
+    prevpagebutton.hidden = false;
+    nextpagebutton.hidden = false;
+}
+function hideDetails():void{
+    returnToOverviewButton.hidden = true;
+    namerow.hidden = true;
+    weightrow.hidden = true;
+    imagerow.hidden = true;
+    abilityrow.hidden = true;
+}
+function showDetails():void{
     returnToOverviewButton.hidden = false;
-
+    namerow.hidden = false;
+    weightrow.hidden = false;
+    imagerow.hidden = false;
+    abilityrow.hidden = false;
+}
+async function getdetails(url:string){
+    const details = await $.get(url);
     namerow.innerHTML = `<td>Name</td><td>${details.name}</td>`;
     weightrow.innerHTML = `<td>Weight</td><td>${details.weight}</td>`;
     imagerow.innerHTML = `<td>Image</td><td><img src="${details.sprites.front_default}"></img></td>`;
@@ -24,16 +43,18 @@ async function getdetails(url:string){
         abilities += `<li>${ability.ability.name}</li>`;
     }
     abilityrow.innerHTML = "<td>Abilities</td><td><ul>" + abilities + "</ul></td>";
-
-    namerow.hidden = false;
-    weightrow.hidden = false;
-    imagerow.hidden = false;
-    abilityrow.hidden = false;
+    hidePokemonlist();
+    showDetails();
 }
-
+async function getpokelist(){
+    const pokelist = await $.get("https://pokeapi.co/api/v2/pokemon/?limit=" + limit + "&offset=" + (page * limit));
+    let html:string = "";
+    for(const pokemon of pokelist.results){
+        html += '<li><span>' + pokemon.name + '</span><button onclick="getdetails(\'' + pokemon.url + '\')">Details</button></li>';
+    }
+    pokemonlist.innerHTML = html;
+}
 $(() => {
-    page = 0;
-    limit = 20;
     pokemonlist = $("#pokemonlist")[0];
     prevpagebutton = <HTMLButtonElement>$("#prevpage")[0];
     nextpagebutton = <HTMLButtonElement>$("#nextpage")[0];
@@ -42,19 +63,8 @@ $(() => {
     weightrow = <HTMLTableRowElement>$("#weightrow")[0];
     imagerow = <HTMLTableRowElement>$("#imagerow")[0];
     abilityrow = <HTMLTableRowElement>$("#abilityrow")[0];
-
     
-    async function getpokelist(){
-        const pokelist = await $.get("https://pokeapi.co/api/v2/pokemon/?limit=" + limit + "&offset=" + (page * limit));
-    
-        let html:string = "";
-        for(const pokemon of pokelist.results){
-            html += '<li><span>' + pokemon.name + '</span><button onclick="getdetails(\'' + pokemon.url + '\')">Details</button></li>';
-        }
-        pokemonlist.innerHTML = html;
-    }
-
-    returnToOverviewButton.hidden = true;
+    hideDetails();
     $("#nextpage").click(function(){
         page++;
         prevpagebutton.disabled = false;
@@ -66,14 +76,8 @@ $(() => {
         getpokelist();
     });
     $("#rto").click(function(){
-        namerow.hidden = true;
-        weightrow.hidden = true;
-        imagerow.hidden = true;
-        abilityrow.hidden = true;
-        returnToOverviewButton.hidden = true;
-        prevpagebutton.hidden = false;
-        nextpagebutton.hidden = false;
-        pokemonlist.hidden = false;
+        hideDetails();
+        showPokemonlist();
     });
     getpokelist();
 });
